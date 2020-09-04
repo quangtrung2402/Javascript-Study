a17cf25fa69f80e1d9c76926a2832eaceb2c3eed
Quang Trung <quangtrung2402@gmail.com>
2020-09-05 00:01:13 +0700

    use debugger

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`Start to study NodeJs`)
+console.log('start Node js')
+debugger;
+var calculateMean = (numbers) => {
+    let total = 0;
+    debugger;
+    numbers.forEach((item) => {
+        total += item;
+        console.log(`total = ${total}, number is ${item}`)
+    })
+    console.log(`Result = ${total}`)
+}
+
+calculateMean([1,2,3,4,5,6,7,8,9])
